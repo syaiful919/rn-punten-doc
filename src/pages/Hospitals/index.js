4fa67a06d982c2c8c7da5045b5c1e81bdd0444f0
syaiful919 <syaiful@cls-indo.com>
2020-08-04 13:23:37 +0700

    dynamic hospital list

--- src/pages/Hospitals/index.js
+++ src/pages/Hospitals/index.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import {ILHospitalBG} from '../../assets/illustration';
-import {fonts, colors} from '../../utils';
+import {fonts, colors, showError} from '../../utils';
 import {ListHospital} from '../../components';
-import {DummyHospital1, DummyHospital2, DummyHospital3} from '../../assets';
+
+import {Fire} from '../../config';
 
 const Hospitals = () => {
+  const [hospitals, setHospitals] = useState([]);
+
+  useEffect(() => {
+    getHospitals();
+  }, []);
+
+  const getHospitals = () => {
+    Fire.database()
+      .ref('hospitals/')
+      .once('value')
+      .then((res) => {
+        if (res.val()) {
+          const data = res.val();
+          const filterData = data.filter((el) => el !== null);
+          setHospitals(filterData);
+        }
+      })
+      .catch((err) => {
+        showError(err.message);
+      });
+  };
+
   return (
     <View style={styles.page}>
       <ImageBackground source={ILHospitalBG} style={styles.background}>
@@ -13,24 +36,17 @@ const Hospitals = () => {
         <Text style={styles.desc}>3 tersedia</Text>
       </ImageBackground>
       <View style={styles.content}>
-        <ListHospital
-          type="Rumah Sakit"
-          name="Citra Bunga Merdeka"
-          address="Jln. Surya Sejahtera 20"
-          pic={DummyHospital1}
-        />
-        <ListHospital
-          type="Rumah Sakit Anak"
-          name="Happy Family Kids"
-          address="Jln. Surya Sejahtera 20"
-          pic={DummyHospital2}
-        />
-        <ListHospital
-          type="Rumah Sakit Jiwa"
-          name="Tingkatan Paling Atas"
-          address="Jln. Surya Sejahtera 20"
-          pic={DummyHospital3}
-        />
+        {hospitals.map((item) => {
+          return (
+            <ListHospital
+              key={`hospital-${item.id}`}
+              type={item.type}
+              name={item.name}
+              address={item.address}
+              pic={{uri: item.image}}
+            />
+          );
+        })}
       </View>
     </View>
   );
